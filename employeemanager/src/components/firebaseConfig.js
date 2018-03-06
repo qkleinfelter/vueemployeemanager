994cc927d1fd90ea8073984b82cfa5e25d97f189
Quinn Kleinfelter <qkleinfelter@sbcglobal.net>
2018-03-06 10:40:56 -0500

    initial firebase setup

---
+++ employeemanager/src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAbk14ihFlesKPehcT1KJkWTapc3NDw508",
+    authDomain: "vuefs-prod-7b72f.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-7b72f.firebaseio.com",
+    projectId: "vuefs-prod-7b72f",
+    storageBucket: "vuefs-prod-7b72f.appspot.com",
+    messagingSenderId: "1042392558730"
+}
