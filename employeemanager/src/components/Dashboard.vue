6d35695aad96e642eda9f52c69465453b3cce968
Quinn Kleinfelter <qkleinfelter@sbcglobal.net>
2018-03-07 10:53:21 -0500

    Finished tut video 3 -- basic functionality on dashboard

--- employeemanager/src/components/Dashboard.vue
+++ employeemanager/src/components/Dashboard.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="dashboard">
-      <h3>Dashboard</h3>
+      <ul class="collection with-header">
+        <li class="collection-header"><h4>Employees</h4></li>
+        <li v-for="employee in employees" v-bind:key="employee.id" class="collection-item"><div class="chip">{{employee.dept}}</div>{{employee.euid}}:{{employee.name}}
+        
+        <router-link class="secondary-content" v-bind:to="{name: 'view-employee', params: {euid: employee.euid}}"><i class="fa fa-eye"></i></router-link>
+        </li>
+      </ul>
 
       <div class="fixed-action-btn">
         <router-link to="/new" class="btn-floating btn-large red">
@@ -11,12 +17,27 @@
 </template>
 
 <script>
+import db from './firebaseInit'
 export default {
   name: 'dashboard',
-  data() {
+  data () {
       return {
         employees: []
       }
+  },
+  created () {
+      db.collection('employees').orderBy('dept').get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          const data = {
+            'id': doc.id,
+            'euid': doc.data().euid,
+            'name': doc.data().name,
+            'dept': doc.data().dept,
+            'position': doc.data().position
+          }
+          this.employees.push(data)
+        })
+      })
   }
 }
 </script>
